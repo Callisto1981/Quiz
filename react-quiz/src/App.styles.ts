0d4efe59205fe5ef4c05807c7de96c91e372ed35
Wayne <wabbruscatoorr@gmail.com>
2021-04-18 13:26:38 -0400

    Importing styled from styled-components, creating GlobalStyle

---
+++ react-quiz/src/App.styles.ts
@@ -0,0 +1,8 @@
+import styled, { createGlobalStyle } from 'styled-components';
+//import BGImage from './images/.....' No image to import
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        height: 100%;
+    }
+    
